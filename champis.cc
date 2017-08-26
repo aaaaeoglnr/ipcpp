df47362907cf1291674570fa474c680eec5d4f11
Octo1a <oana.garlea@gmail.com>
2017-08-26 15:55:20 +0300

    Correct French characters

--- champis.cc
+++ champis.cc
@@ -1,15 +1,15 @@
-  #include<iostream>
-  using namespace std;
+#include<iostream>
+using namespace std;
   
-  int main()
-  {
-	  int anneau, foret, convexe, lamelles;
-  cout << "Pensez Ã  un champignon : amanite tue-mouches, pied bleu, girolle," << endl
-       << "cÃ¨pe de Bordeaux, coprin chevelu ou agaric jaunissant." << endl << endl;
+int main()
+{
+  int anneau, foret, convexe, lamelles;
+  cout << "Pensez à un champignon : amanite tue-mouches, pied bleu, girolle," << endl
+       << "cèpe de Bordeaux, coprin chevelu ou agaric jaunissant." << endl << endl;
   cout << "Est-ce que votre champignon a un anneau (1 : oui, 0 : non) ? ";
   cin >> anneau;
   if (anneau){
-	  cout << "Est-ce que votre champignon vit en forÃªt (1 : oui, 0 : non) ? ";
+	  cout << "Est-ce que votre champignon vit en forêt (1 : oui, 0 : non) ? ";
 	  cin >> foret;
 	  if (foret){
 		  cout << "==> Le champignon auquel vous pensez est ";
@@ -34,7 +34,7 @@
 		if (lamelles) {
 			cout << "la girolle";
 		} else {
-		 cout << "le cÃ¨pe de Bordeaux";
+			cout << "le cèpe de Bordeaux";
 		}
 	  }
 		  
