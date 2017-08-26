dc47f9b2eac5d8b5b38d3ac4cfbb4598cefebc81
Octo1a <oana.garlea@gmail.com>
2017-08-26 14:03:35 +0300

    Add assignm02 part1

---
+++ location_velos.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Donnez l'heure de début de la location (un entier) : ";
+  int debut;
+  cin >> debut;
+
+  cout << "Donnez l'heure de fin de la location (un entier) : ";
+  int fin;
+  cin >> fin;
+
+  /*****************************************************
+   * Compléter le code à partir d'ici
+   *****************************************************/
+	if ((debut < 0) or (debut > 24) or (fin < 0) or (fin > 24))  
+    cout << "Les heures doivent être comprises entre 0 et 24 !" << endl;
+    else if (debut == fin)
+    cout << "Bizarre, vous n'avez pas loué votre vélo bien longtemps !" 
+    << endl;
+    else if (debut > fin)
+    cout << "Bizarre, le début de la location est après la fin ..." 
+    << endl;
+    else{
+			int heures1 = fin - debut; 
+			int heures2=0;  
+			if (fin > 7){ 
+				if (fin < 17) { 
+					if (debut < 7){
+						heures2 = fin - 7;				
+					} else {
+						heures2 = fin - debut;
+					}
+				} else if (debut < 7) {
+					heures2 = 17-7;
+				} else if (debut < 17){
+						heures2 = 17 - debut;
+				} 
+			heures1 -= heures2;
+			} 
+		int total = heures1 * 1 + heures2 * 2;
+		cout << "Vous avez loué votre vélo pendant" << endl;
+		cout << heures1;
+		cout << " heure(s) au tarif horaire de "
+           << "1 franc(s)" << endl;
+		cout << heures2;
+		cout << " heure(s) au tarif horaire de "
+           << "2 franc(s)" << endl;
+		cout << "Le montant total à payer est de "
+         << total << " franc(s)." << endl;
+	}
+  /*******************************************
+   * Ne rien modifier après cette ligne.
+   *******************************************/
+
+  return 0;
+}
